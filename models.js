0d947f9805def39026492bac124369e3248c478c
Ahmed Khyry <ahmedkhyry1@gmail.com>
2023-02-12 15:04:40 +0200

    edit dev

--- models.js
+++ models.js
@@ -2,53 +2,32 @@ const mongoose = require('mongoose');
 
 // Schemas
 const UserSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    gender: {
-        type: String,
-        required: true
-    },
-    userImage: {
-        type: String,
-        required: false
-    }
-});
-
-const productSchema = mongoose.Schema({
-    name: {
-        type: String,
-        required: [true, 'Please add a name'],
-        unique: true,
-        trim: true,
-        maxlength: [50, 'Name can not be more than 50 characters']
-    },
-    price: {
-        type: Number,
-        required: [true, 'Please add a price']
-    },
-    description: {
-        type: String,
-        required: [true, 'Please add a description'],
-        maxlength: [200, 'Description can not be more than 200 characters']
-    }
+  username: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  gender: {
+    type: String,
+    required: true
+  },
+  userImage: {
+    type: String,
+    required: false
+  }
 });
 
 // Models
-const User = mongoose.model('User', UserSchema)
-const Product = mongoose.model('Product', productSchema)
+const User = mongoose.model('User', UserSchema);
 
 // export all models created
 module.exports = {
-    User,
-    Product
-}
+  User
+};
