ad8189b3c3007caa8526af8f1275bdce0a9b98d7
AbdallahNagy <abdallahnagy773@gmail.com>
2023-02-20 08:32:33 +0200

    fix purchase bugs

--- routers/purchase.js
+++ routers/purchase.js
@@ -1,60 +1,46 @@
-const util = require('util')
-const express = require('express')
-const customError = require('../customError')
-const Order = require('../models/orderModel')
-const User = require('../models/userModel');
-const Product = require('../models/productModel');
-const purchaseRouter = express.Router()
+const util = require("util");
+const express = require("express");
+const customError = require("../customError");
+const Order = require("../models/orderModel");
+const User = require("../models/userModel");
+const Product = require("../models/productModel");
+const purchaseRouter = express.Router();
 
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 const verifyAsync = util.promisify(jwt.verify);
-const secretKey = process.env.SECRET_KEY || 'hgdnckhnd';
+const secretKey = process.env.SECRET_KEY || "hgdnckhnd";
 
-purchaseRouter.post('/', async (req, res, next) => {
+purchaseRouter.post("/", async (req, res, next) => {
     try {
-        const productIds = req.body // array of product ids
-        const { authorization: token } = req.headers
+        const arrOfOrders = req.body;
+        const { authorization: token } = req.headers;
         const payload = await verifyAsync(token, secretKey);
-        const userId = payload.id
+        const userId = payload.id;
 
         // get user
-        const user = await User.findById(userId)
+        const user = await User.findById(userId);
 
         let order;
         // create orders for user
-        for (let i = 0; i < productIds.length; i++) {
-            const productExist = await Order.findOne({ productId: productIds[i] , userId})
-            if (productExist) {
+        for (let i = 0; i < arrOfOrders.length; i++) {
+            order = await Order.create({
+                userId,
+                productId: arrOfOrders[i]._id,
+                status: "pending",
+                amount: arrOfOrders[i].quantity,
+                totalPrice: arrOfOrders[i].total,
+            });
 
-                const pro = await productExist.populate('productId')                
-                console.log(pro)
-                productExist.amount++;
-                productExist.totalPrice = productExist.totalPrice + pro.productId.price;
-                await productExist.save();
-            } else {
-                const product = await Product.findById(productIds[i]);
-
-                order = await Order.create({
-                    userId,
-                    productId: productIds[i],
-                    status: 'pending',
-                    amount: 1,
-                    totalPrice: product.price
-                })
-
-                // push order id to user orders
-                user.orders.push(order._id)
-                await user.save()
-            }
+            // push order id to user orders
+            user.orders.push(order._id);
+            await user.save();
         }
 
-        const userOrders = await user.populate('orders')
-        res.status(200).send(userOrders.orders)
-
+        const userOrders = await user.populate("orders");
+        res.status(200).send(userOrders.orders);
     } catch (error) {
-        next(error)
+        next(error);
     }
+});
 
-})
-
-module.exports = purchaseRouter
+module.exports = purchaseRouter;
